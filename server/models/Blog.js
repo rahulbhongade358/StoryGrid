176208f5260fdcc37cfe4e44e8e4fb1be2944f39
rahulbhongade358 <rahulbhongade912@gmail.com>
2025-09-20 17:25:11 +0530

    feat:implimented Models and schema

---
+++ server/models/Blog.js
@@ -0,0 +1,14 @@
+import {model,Schema} from "mongoose";
+
+const blogSchema = new Schema({
+    title:{type:String,required:true},
+    content:{type:String,required:true},
+    status:{type:String,default:"DRAFT",enum:["DRAFT","PUBLISHED","ARCHIVED"]},
+    category:{type:String,required:true},
+    publishedAt:{type:Date},
+    author:{type:Schema.Types.ObjectId,ref:"User",required:true}
+},{timestamps:true});
+
+const Blog = model("Blog",blogSchema);
+
+export default Blog;
---
+++ server/models/User.js
@@ -0,0 +1,12 @@
+import {model,Schema} from "mongoose";
+
+const userSchema = new Schema({
+    username:{type:String,required:true},
+    email:{type:String,required:true,unique:true},
+    password:{type:String,required:true}
+},{timestamps:true}
+);
+
+const User = model("User",userSchema);
+
+export default User;
